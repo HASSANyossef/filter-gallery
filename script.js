243bb2d75d2a5a26d38e4054a10296541d2c9e41
HASSANyossef <88420194+HASSANyossef@users.noreply.github.com>
2021-11-24 22:04:28 +0300

    filter gallery

--- script.js
+++ script.js
@@ -1,29 +1,107 @@
+let allBtn = document.querySelector(".all");
+let cameraBtn = document.querySelector(".cameras");
+let headphoneBtn = document.querySelector(".headphones");
+let mobileBtn = document.querySelector(".mobiles");
+let shoeBtn = document.querySelector(".shoes");
+let watchBtn = document.querySelector(".watchs");
 
-var img= document.getElementsByClassName("img")
+let cameraItems = document.querySelector(".camera");
+let headphoneItems = document.querySelector(".headphone");
+let mobileItems = document.querySelector(".mobile");
+let shoeItems = document.querySelector(".shoe");
+let watchItems = document.querySelector(".watch");
 
-function getImg(){
-    for (let i= 0; i<img.length; i++) {
-        document.getElementsByClassName("img")[i];
-    }
+let loading = document.querySelector(".loading");
+let gallery = document.querySelector(".fi-gallery");
+
+
+
+function stopLoading()
+{
+    loading.style.display = "none";
 }
 
-var camera= document.getElementsByClassName("camera");
+function loadTime()
+{
+    setInterval(stopLoading, 5000);
+}
+window.onload = loadTime();
 
-function getCamera(){
-    var temp='';
-    for (let i= 0; i<camera.length-1; i++) {
-        temp+= camera[i];
-    }
-    document.getElementById("count").innerHTML=temp;
+
+function showItimes()
+{
+    gallery.style.transform = "scale(1)";
+}
+function items()
+{
+    setInterval(showItimes, 5000);
+}
+window.onload = items();
+
+
+allBtn.addEventListener("click",showAll)
+
+function showAll()
+{
+    cameraItems.style.display = "flex";
+    headphoneItems.style.display = "flex";
+    mobileItems.style.display = "flex";
+    shoeItems.style.display = "flex";
+    watchItems.style.display = "flex";
 }
 
 
-document.getElementsByClassName("btn")[0].addEventListener("click",function(){
-    getImg();
-})
+cameraBtn.addEventListener("click", showCamera)
 
+function showCamera()
+{
+    cameraItems.style.display = "flex";
+    headphoneItems.style.display = "none";
+    mobileItems.style.display = "none";
+    shoeItems.style.display = "none";
+    watchItems.style.display = "none";
+}
 
-document.getElementsByClassName("btn")[1].addEventListener("click",function(){
-    getCamera();
-    
-})
+headphoneBtn.addEventListener("click", showHeadphone)
+
+function showHeadphone()
+{
+    headphoneItems.style.display = "flex";
+    cameraItems.style.display = "none";
+    mobileItems.style.display = "none";
+    shoeItems.style.display = "none";
+    watchItems.style.display = "none";
+}
+
+mobileBtn.addEventListener("click", showMobile)
+
+function showMobile()
+{
+    mobileItems.style.display = "flex";
+    headphoneItems.style.display = "none";
+    cameraItems.style.display = "none";
+    shoeItems.style.display = "none";
+    watchItems.style.display = "none";
+}
+
+shoeBtn.addEventListener("click", showShoe)
+
+function showShoe()
+{
+    shoeItems.style.display = "flex";
+    headphoneItems.style.display = "none";
+    mobileItems.style.display = "none";
+    cameraItems.style.display = "none";
+    watchItems.style.display = "none";
+}
+
+watchBtn.addEventListener("click", showWatch)
+
+function showWatch()
+{
+    watchItems.style.display = "flex";
+    headphoneItems.style.display = "none";
+    mobileItems.style.display = "none";
+    shoeItems.style.display = "none";
+    cameraItems.style.display = "none";
+}
